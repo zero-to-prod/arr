67fe971822741b81d89b6792639aa5427f3877d2
david_smith <david_smith@sweetwater.com>
2025-02-03 21:22:29 -0500

    feat/arr Renamed `tests/Unit/ExampleTest.php` to `tests/Unit/MapKeysTest.php`.

---
+++ tests/Unit/MapKeysTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Zerotoprod\Arr\Arr;
+
+class MapKeysTest extends TestCase
+{
+    /** @test */
+    public function map_keys(): void
+    {
+        $array = [
+            'Key1' => [
+                'Key2' => 1
+            ]
+        ];
+
+        $new_array = Arr::mapKeys($array, function (string $key) {
+            return strtolower($key);
+        });
+
+        self::assertEquals(1, $new_array['key1']['key2']);
+    }
+}
